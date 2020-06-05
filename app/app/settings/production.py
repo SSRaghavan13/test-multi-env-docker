e525515dbbce9886d712c75edfd7ef3d07180503
SSRaghavan13 <ssraghavan25@gmail.com>
2020-06-05 10:47:04 -0700

    First commit

---
+++ app/app/settings/production.py
@@ -0,0 +1,11 @@
+from .base import *
+
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}

4cc9ef9dfae55387ae684d3d8920005b9c1a11bc
SSRaghavan13 <ssraghavan25@gmail.com>
2020-06-09 13:05:04 -0700

    pushing to ECR

--- deploy.sh
+++ deploy.sh
@@ -17,13 +17,13 @@ sudo chmod +x /usr/bin/ecs-deploy
 eval $(aws ecr get-login --region us-east-2)
 
 # or login DockerHub
-docker login -u=${DOCKERHUB_USERNAME} -p=${DOCKERHUB_PASSWORD}
+# docker login -u=${DOCKERHUB_USERNAME} -p=${DOCKERHUB_PASSWORD}
 
-docker pull ssraghavan13/recipe-app-api:latest
+# docker pull ssraghavan13/recipe-app-api:latest
 
-# build the docker image and push to an image repository
-docker tag ssraghavan13/recipe-app-api:latest 266509442025.dkr.ecr.us-east-2.amazonaws.com/recipe-app-aws:latest
-docker push 266509442025.dkr.ecr.us-east-2.amazonaws.com/recipe-app-aws:latest
+# # build the docker image and push to an image repository
+# docker tag ssraghavan13/recipe-app-api:latest 266509442025.dkr.ecr.us-east-2.amazonaws.com/recipe-app-aws:latest
+# docker push 266509442025.dkr.ecr.us-east-2.amazonaws.com/recipe-app-aws:latest
 
 # update an AWS ECS service with the new image
 ecs-deploy -c recipe-cluster -n recipe-service -i 266509442025.dkr.ecr.us-east-2.amazonaws.com/recipe-app-aws:latest
--- pull_script.sh
+++ pull_script.sh
@@ -17,3 +17,6 @@ docker-compose -f docker-compose.test.yaml run app sh -c "python manage.py test"
 docker build -t ssraghavan13/recipe-app-api .
 docker login -u=${DOCKERHUB_USERNAME} -p=${DOCKERHUB_PASSWORD}
 docker push ssraghavan13/recipe-app-api:latest
+
+docker tag ssraghavan13/recipe-app-api:latest 266509442025.dkr.ecr.us-east-2.amazonaws.com/recipe-app-aws:latest
+docker push 266509442025.dkr.ecr.us-east-2.amazonaws.com/recipe-app-aws:latest
